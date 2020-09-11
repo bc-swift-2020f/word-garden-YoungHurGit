722beef3f8cd3c7a26b6d73e6d6c641f0258982b
Young Hur <huryo@bc.edu>
2020-09-11 12:38:23 -0400

    Initial Commit

---
+++ WordGarden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGarden
+//
+//  Created by yhur18 on 9/11/20.
+//  Copyright © 2020 Young Hur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
